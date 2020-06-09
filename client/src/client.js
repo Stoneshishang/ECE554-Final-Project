b334e0fa0588ec305688812c63ab700e20591181
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-06-09 16:19:27 -0400

    updated version with both betting price and time

--- client/src/client.js
+++ client/src/client.js
@@ -1,3 +1,5 @@
+let sock = io();
+
 const writeEvent = (text) => {
   //<ul> element
   const parent = document.querySelector("#events");
@@ -20,31 +22,51 @@ const onFormSubmitted = (e) => {
   sock.emit("message", text);
 };
 
-// Countdown Timer
-// const startingTime = 10;
-
-// const countdownElement = document.getElementById("countdown");
-// setInterval(updateCountdown, 1000);
-
-// const updateCountdown = () => {
-//   let seconds = startingTime;
-
-//   countdownElement.innerHTML = `00:${seconds}`;
-//   startingTime--;
-// };
-
 // Display the inputted price
 const bettingPrice = () => {
-  const price = document.getElementById(price);
+  const price = document.getElementById("price");
   price.addEventListener("keyup", (e) => {
     if (e.key === 13) {
       event.preventDefault();
       const input = document.querySelector("#price");
       // const text = input.value;
 
-      sock.emit("price", input);
+      console.log(`betting price input is ${input}`);
+
+      sock.emit("turn", `You have selected betting price of ${input}`);
     }
   });
+};
+
+const bettingPriceForm = (e) => {
+  e.preventDefault();
+
+  const inputVal = document.querySelector("#price");
+
+  sock.emit("message", inputVal.value);
+};
+
+// Display the Betting Time
+const bettingTime = () => {
+  const price = document.getElementById("time");
+  price.addEventListener("keyup", (e) => {
+    if (e.key === 13) {
+      event.preventDefault();
+      const selection = document.querySelector("#time");
+      // const text = input.value;
+
+      console.log(`betting time input is ${selection}`);
+      sock.emit("turn", selection);
+    }
+  });
+};
+
+const bettingTimeForm = (e) => {
+  e.preventDefault();
+
+  const inputVal = document.querySelector("#time");
+
+  sock.emit("message", inputVal.value);
 };
 
 //Display the selected betting amount
@@ -57,14 +79,29 @@ const addPriceButtonListeners = () => {
   });
 };
 
-writeEvent("Welcome to RT Gambling!");
+const main = () => {
+  writeEvent("Welcome to RT Gambling!");
 
-const sock = io();
-sock.on("message", writeEvent);
+  const sock = io();
+  sock.on("message", writeEvent);
 
-document
-  .querySelector("#chat-form")
-  .addEventListener("submit", onFormSubmitted);
+  document
+    .querySelector("#chat-form")
+    .addEventListener("submit", onFormSubmitted);
 
-addPriceButtonListeners();
-bettingPrice();
+  document
+    .querySelector("#betting-price-form")
+    .addEventListener("submit", bettingPriceForm);
+
+  document
+    .querySelector("#time-form")
+    .addEventListener("submit", bettingTimeForm);
+
+  addPriceButtonListeners();
+  bettingPrice();
+  bettingTime();
+};
+
+document.onreadystatechange = () => {
+  if (document.readyState === "complete") main();
+};
